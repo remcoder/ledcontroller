cef1b11f5fc15e24652164f323ce7805dae7839e
Remco Veldkamp <remcoder@gmail.com>
2016-08-14 09:23:11 +0200

    Upgrade to nodemcu 1.5.1.4

--- flash.sh
+++ flash.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 export ESPTOOL_PORT=/dev/cu.SLAB_USBtoUART
-esptool.py write_flash 0x00000 firmware.bin
+export ESPTOOL_FS=32m # 32m works
+export ESPTOOL_FM=qio # qio works
+FIRMWARE=nodemcu-master-8-modules-2016-08-10-17-47-53-integer.bin
+
+esptool.py write_flash --verify 0x00000 $FIRMWARE 0x3fc000 esp_init_data_default.bin
